de66ddea5686e1d30938333f650d8217317e717a
jkkwjk <465640448@qq.com>
2019-12-03 12:15:12 +0800

    finish 微信小程序添加假单的表单效验

--- wechat/pages/main/student/applyMain/applyMain.js
+++ wechat/pages/main/student/applyMain/applyMain.js
@@ -1,9 +1,5 @@
-// pages/main/student/addOrChangeApply/applyMain.js
+import dateUtil from "../../../../utils/datautil.js"
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
     title: '',
     row: {},
@@ -13,16 +9,22 @@ Page({
     },
     form: {
       type: '',
+      date: {
+        start: '',
+        end: '',
+      },
+      time: {
+        start: '00:00',
+        end: '00:00',
+      },
+      detail: '',
     }
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
   onLoad: function (options) {
     const now = new Date();
     this.setData({
-      "now.date": now.getFullYear+'-'+now.getMonth+1+'-'+now.getDate();
+      "now.date": now.getFullYear()+'-'+(parseInt(now.getMonth())+1)+'-'+now.getDate()
     });
     if (options.row === undefined){
       this.setData({
@@ -35,19 +37,6 @@ Page({
     }
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function (e) {
-    
-  },
   showLeaveType() {
     this.setData({
       showLeaveType: true
@@ -63,5 +52,63 @@ Page({
       "form.type": e.currentTarget.dataset.type,
     });
     this.closeLeaveType();
+  },
+  detailChange(e){
+    this.setData({
+      "form.detail": e.detail.value
+    });
+    
+  },
+  dateChange(e){
+    const dataset = e.currentTarget.dataset;
+    if (dataset.what === 'date'){
+      if (dataset.range === 'start'){
+        this.setData({
+          "form.date.start" :e.detail.value
+        });
+      }else{
+        this.setData({
+          "form.date.end": e.detail.value
+        });
+      }
+    }else{
+      if (dataset.range === 'start') {
+        this.setData({
+          "form.time.start": e.detail.value
+        });
+      } else {
+        this.setData({
+          "form.time.end": e.detail.value
+        });
+      }
+    }
+  },
+  verifyForm(){
+    const form = this.data.form;
+    if (form.type === '')
+      return '请假类型不能为空';
+    if (form.detail === '')
+      return '请假原因不能为空';
+    if (form.date.start === '')
+      return '开始日期不能为空';
+    if (form.date.end === '')
+      return '结束日期不能为空';
+    const startTime = form.date.start + ' ' + form.time.start;
+    const startTimeD = dateUtil.fixTimezoneOffset(dateUtil.parse(startTime));
+    const endTime = form.date.end + ' ' + form.time.end;
+    const endTimeD = dateUtil.fixTimezoneOffset(dateUtil.parse(endTime));
+    if (startTimeD >= endTimeD)
+      return '开始日期不能大于结束日期';
+
+    if (endTimeD < new Date())
+      return '结束时间超过当前日期';
+  },
+  submit(){
+    const tip = this.verifyForm();
+    if(tip === undefined){
+      console.log("成功");
+    }else{
+      console.log(tip);
+    }
   }
 })
