c35dc0793c6c5ffc46e6b4a1248ccb83a404dd52
jkkwjk <465640448@qq.com>
2019-12-03 12:17:57 +0800

    fix 大小写git不能识别

---
+++ wechat/components/tableMain/tableMain.js
@@ -0,0 +1,29 @@
+// components/tableMain/TableMain.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    column: {
+      require: true,
+      type: Array
+    },
+    tableData: {
+      require: true,
+      type: Array
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    
+  }
+})
